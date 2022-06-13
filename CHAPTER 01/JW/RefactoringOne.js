cc2bd0123c3f8d50d4c4e0c42516f9b9acfd9b5b
ljw0096 <ljw0096@naver.com>
2022-06-14 04:54:05 +0900

    refactor: TotalAmount, TotalVolumneCredits 함수 statement로 이동

--- CHAPTER 01/JW/RefactoringOne.js
+++ CHAPTER 01/JW/RefactoringOne.js
@@ -2,12 +2,45 @@ export function statement(invoice, plays) {
   const statementData = {}
   statementData.customer = invoice.customer
   statementData.performances = invoice.performances.map(enrichPerformance)
+  statementData.totalAmount = totalAmount(statementData)
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData)
   return renderPlainText(statementData, plays)
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance)
     result.play = playFor(result)
     result.amount = amountFor(result)
+    result.volumeCredits = volumeCreditsFor(result)
+    return result
+  }
+
+  function totalAmount(data) {
+    let result = 0
+
+    for (let perf of data.performances) {
+      result += perf.amount
+    }
+    return result
+  }
+
+  function totalVolumeCredits(data) {
+    let result = 0
+
+    for (let perf of data.performances) {
+      result += perf.volumeCredits
+    }
+    return result
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0
+
+    result += Math.max(aPerformance.audience - 30, 0)
+
+    if ('comedy' === aPerformance.play.type) {
+      result += Math.floor(aPerformance.audience / 5)
+    }
+
     return result
   }
 
@@ -55,41 +88,11 @@ export function renderPlainText(data, plays) {
 
   return result
 
-  function totalAmount() {
-    let result = 0
-
-    for (let perf of data.performances) {
-      result += perf.amount
-    }
-    return result
-  }
-
-  function totalVolumeCredits() {
-    let result = 0
-
-    for (let perf of data.performances) {
-      result += volumeCreditsFor(perf)
-    }
-    return result
-  }
-
   function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       maximumFractionDigits: 2,
     }).format(aNumber / 100)
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0
-
-    result += Math.max(aPerformance.audience - 30, 0)
-
-    if ('comedy' === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5)
-    }
-
-    return result
   }
 }
