49568d7bf162c8420a06175c645fdedb8f15a8a8
ljw0096 <ljw0096@naver.com>
2022-06-14 05:18:25 +0900

    refactor: amountFor 함수 calculator method로 변환

--- CHAPTER 01/JW/createStatementData.js
+++ CHAPTER 01/JW/createStatementData.js
@@ -1,6 +1,33 @@
 class PerformanceCalculator {
-  constructor(aPerformance) {
+  constructor(aPerformance, aPlay) {
     this.performance = aPerformance
+    this.play = aPlay
+  }
+
+  get amount() {
+    let result = 0
+
+    switch (this.play.type) {
+      case 'tragedy':
+        result = 40_000
+
+        if (this.performance.audience > 30) {
+          result += 1_000 * (this.performance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30_000
+
+        if (this.performance.audience > 20) {
+          result += 10_000 + 500 * (this.performance.audience - 20)
+        }
+        result += 300 * this.performance.audience
+        break
+
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`)
+    }
+    return result
   }
 }
 
@@ -13,9 +40,12 @@ export default function createStatementData(invoice, plays) {
   return statementData
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance)
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    )
     const result = Object.assign({}, aPerformance)
-    result.play = playFor(result)
+    result.play = calculator.play
     result.amount = amountFor(result)
     result.volumeCredits = volumeCreditsFor(result)
     return result
@@ -46,28 +76,6 @@ export default function createStatementData(invoice, plays) {
   }
 
   function amountFor(aPerformance) {
-    let result = 0
-
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40_000
-
-        if (aPerformance.audience > 30) {
-          result += 1_000 * (aPerformance.audience - 30)
-        }
-        break
-      case 'comedy':
-        result = 30_000
-
-        if (aPerformance.audience > 20) {
-          result += 10_000 + 500 * (aPerformance.audience - 20)
-        }
-        result += 300 * aPerformance.audience
-        break
-
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
-    }
-    return result
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount
   }
 }
